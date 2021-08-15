11289fa67cc7585588f9ec65205e667f5bae1293
OniHexx0ff <franciskaster86@gmail.com>
2021-08-14 22:37:24 -0300

    1.0

---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home">
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">Bem vindo ao ComercioMestre</p>
+        <p class="subtitle">A melhor loja com só duas opções do mundo</p>
+      </div>
+    </section>
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">ultimos produtos</h2>
+      </div>
+      <ProductBox
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+        v-bind:product="product"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductBox from "@/components/ProductBox";
+import axios from "axios";
+export default {
+  name: "Home",
+  data() {
+    return {
+      latestProducts: [],
+    };
+  },
+  components: {
+    ProductBox,
+  },
+  mounted() {
+    this.getLatestProducts();
+    document.title = "Início | CM";
+  },
+  methods: {
+    async getLatestProducts() {
+      this.$store.commit("setIsLoading", true);
+      await axios
+        .get("/api/v1/latest-products/")
+        .then((response) => {
+          this.latestProducts = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      this.$store.commit("setIsLoading", false);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
